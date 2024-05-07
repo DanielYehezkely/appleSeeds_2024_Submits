bc71f3d9655acfaba8bd35688b9cc1cdad7b3c49
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-07 15:38:46 +0300

    finished all hoisting exercises

---
+++ Exercises/JS/Scope and Hoisitng/script.js
@@ -0,0 +1,28 @@
+// Quiz
+// 1.d
+// 2.d
+// 3.a
+// 4.c
+// 5.d
+// 6.a
+// 7.d
+
+// Scope and Hoisting Exercises
+// exercise 1 - Basic Understanding of Hoisting
+// undefine, reference error on both let and const
+
+// exercise 2 - Understanding Function Hoisting
+// function declaration can be called from anywhere, so the answer is "Hello from function declaration!"
+// function expression will be undefine since its declared using var we will get typeerror
+
+// exercise 3 - Nested Scopes and Hoisting
+// console.log(x); - global
+// console.log(y); - outer
+// console.log(z); - reference error
+
+// exercise 4 - Combining All Concepts
+// console.log(a) - 10
+// console.log(a) - 1
+// console.log(x) - undefine
+// console.log(x) - refrence error
+
