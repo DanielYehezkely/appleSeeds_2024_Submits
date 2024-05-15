b0dbd16ec3da1795a00d618ca1312167dddb76c1
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-15 13:31:44 +0300

    finished the exercise 6

---
+++ Exercises/JS/DOM/Exercise 6 - table manipulation/script.js
@@ -0,0 +1,21 @@
+const rows = document.querySelectorAll('#dataTable tr');
+
+const toggleColumn = (index) => {
+    rows.forEach(row => {
+        const cell = row.cells[index]
+        if (cell) {
+            cell.style.display = cell.style.display === 'none' ? '' : 'none';
+        }
+    })
+}
+
+const resetTable = () => {
+    rows.forEach(row => {
+        Array.from(row.cells).forEach(cell => {
+            cell.style.display = ''
+        })
+    })
+}
+
+
+
