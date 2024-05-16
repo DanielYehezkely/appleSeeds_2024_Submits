db9834e6b3f70f064f99f9567029cb3ffddf3f3b
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-16 13:33:50 +0300

    finished exercise 9

---
+++ Exercises/JS/Event listeners/Exercise 9 - shoping cart with closures/script.js
@@ -0,0 +1,33 @@
+const cart = {}
+const cartItems = document.querySelector('.cart-items')
+
+function addProductToCart(productId) {
+    return function () {
+        if (cart[productId]) {
+            cart[productId]++;
+        } else {
+            cart[productId] = 1
+        }
+        updateCart()
+    }
+}
+
+function updateCart() {
+    cartItems.innerHTML = ''
+    for (const [productId, quantity] of Object.entries(cart)) {
+        const cartItemDiv = document.createElement('div');
+        cartItemDiv.className = 'cart-item';
+        cartItemDiv.innerHTML = `Product ${productId}, Quantity: ${quantity}`;
+        cartItems.appendChild(cartItemDiv);
+    }
+}
+
+const productButtons = document.querySelectorAll('.product button');
+productButtons.forEach(button => {
+    const productId = button.parentElement.getAttribute('data-id');
+    button.addEventListener('click', addProductToCart(productId));
+});
+
+
+
+
